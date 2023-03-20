e77c364359e6cdc69984d152ef7e0c480fbd0b59
chelizichen <1347290221@qq.com>
2023-03-20 15:32:31 +0800

    update:ts-5.0.2

--- decorator/web/aop/index.ts
+++ decorator/web/aop/index.ts
@@ -2,19 +2,16 @@ import { Request,Response } from "express";
 
 function UseInterCeptor(interceptor:TarsusInterCeptor){
     return function (value:any,context:ClassMethodDecoratorContext){
-        let copy_value = value;
-        value = async function (req:Request){
-            // @ts-ignore
-            const data =  await interceptor.handle.call(this,req)
-            if(data){
-                return data;
-            }else{
-            // @ts-ignore
-                const data = await copy_value.call(this,req)
+        async function intercepotorFn<This = unknown>(this: This, ...args: any[]) {
+            const data = await interceptor.handle.call(this, ...args)
+            if (data) {
+                return data
+            } else {
+                let data = value.call(this, ...args)
                 return data
             }
-        }
-        return value
+        };
+        return intercepotorFn;
     }
 }
 
