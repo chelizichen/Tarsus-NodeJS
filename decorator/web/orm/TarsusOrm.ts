f05620c7a689335b7efd45d593df0cf2be9e0781
chelizichen <1347290221@qq.com>
2023-06-06 09:40:23 +0800

    symbol 构建私有变量

--- decorator/web/orm/TarsusOrm.ts
+++ decorator/web/orm/TarsusOrm.ts
@@ -3,48 +3,52 @@ import _ from "lodash";
 import { __column__ } from "./Entity";
 import {OrmMethods, Pagination} from './Repo';
 import { SQLTools } from "./Tools";
+
+const PrepareToQuery = Symbol.for('PrepareToQuery')
+
 /**
  * @description Tarsus ORM 对象与实体映射类
  * @type {OrmMethods}
  */
-class TarsusOrm<T = any> implements OrmMethods<T>{
+class TarsusOrm<T = any> implements OrmMethods<T> {
   /**
    * @description 重载getList
    */
- async getList(pagination?: Pagination): Promise<T[]>;
- async getList(options?: Record<string, any>, pagination?: Pagination):Promise<T[]>;
- async getList(options?: unknown, pagination?: unknown): Promise<T[]> {
-    const sql = new SQLTools(this.constructor.prototype)
-    if(options){
-      
+  async getList(pagination?: Pagination): Promise<T[]>;
+  async getList(
+    options?: Record<string, any>,
+    pagination?: Pagination
+  ): Promise<T[]>;
+  async getList(options?: unknown, pagination?: unknown): Promise<T[]> {
+    const sql = new SQLTools(this.constructor.prototype);
+    if (options) {
       // 单纯走分页
-      if(options instanceof Pagination){
-        const _sql_ = sql.pagination(options).getSQL()
+      if (options instanceof Pagination) {
+        const _sql_ = sql.pagination(options).getSQL();
         console.log(_sql_);
-        
-        const data = await this.prepare_to_query<T[]>(_sql_)
-        return data
-      }// 如果走有参数的
-      else if (typeof(options) == "object" && options != null){
-        sql.buildWhere(options)
+
+        const data = await this[PrepareToQuery]<T[]>(_sql_);
+        return data;
+      } // 如果走有参数的
+      else if (typeof options == "object" && options != null) {
+        sql.buildWhere(options);
         // 如果有分页参数
-        if(pagination instanceof Pagination){
+        if (pagination instanceof Pagination) {
           console.log(sql);
-          const _sql_ = sql.pagination(options).getSQL()
-          const data = await this.prepare_to_query<T[]>(_sql_)
-          return data
-        }// 走无分页参数
-        else{
-          const _sql_ = sql.getSQL()
-          const data = await this.prepare_to_query<T[]>(_sql_)
-          return data
+          const _sql_ = sql.pagination(options).getSQL();
+          const data = await this[PrepareToQuery]<T[]>(_sql_);
+          return data;
+        } // 走无分页参数
+        else {
+          const _sql_ = sql.getSQL();
+          const data = await this[PrepareToQuery]<T[]>(_sql_);
+          return data;
         }
       }
-    }// 直接走无参拿所有
-    else{
-      return null
+    } // 直接走无参拿所有
+    else {
+      return null;
     }
-
   }
   async findOne(id: string | number): Promise<T> {
     throw new Error("Method not implemented.");
@@ -86,14 +90,14 @@ class TarsusOrm<T = any> implements OrmMethods<T>{
   async query(sql: string, args: any[] = []): Promise<any> {
     const vm = TarsusOrm;
     const tgvm = this.constructor.prototype;
-    
+
     return new Promise(async (resolve, reject) => {
       vm.connectionPool.getConnection((err, conn) => {
         if (err) {
           reject(err);
         }
-        console.log(sql,args);
-        
+        console.log(sql, args);
+
         conn.query(sql, args, function (err, resu) {
           if (err) {
             reject(err);
@@ -101,10 +105,10 @@ class TarsusOrm<T = any> implements OrmMethods<T>{
           if (resu && resu.length) {
             // const fields = EntityMap.get(targetEntity.name) as Map<string, string>;
             const __columns__ = tgvm.__columns__;
-            
+
             // mapper orm
             const data = resu.map((item) => {
-              const __item__ = {}
+              const __item__ = {};
               for (let v in __columns__) {
                 let { column_name } = __columns__[v];
                 __item__[column_name] = item[v];
@@ -119,9 +123,9 @@ class TarsusOrm<T = any> implements OrmMethods<T>{
       });
     });
   }
-  async prepare_to_query<T>(sql: string, args: any[] = []):Promise<T>{
+  async [PrepareToQuery]<T>(sql: string, args: any[] = []): Promise<T> {
     const vm = TarsusOrm;
-    return new Promise( async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       vm.connectionPool.getConnection((err, conn) => {
         if (err) {
           reject(err);
@@ -130,13 +134,11 @@ class TarsusOrm<T = any> implements OrmMethods<T>{
           if (err) {
             reject(err);
           }
-          resolve(resu)
-        })
-      })
-    })
+          resolve(resu);
+        });
+      });
+    });
   }
-  
-
 
   // static async queryTest(sql:string){
   //   const data = await ArcOrm.ConnectionPool.query(sql);
